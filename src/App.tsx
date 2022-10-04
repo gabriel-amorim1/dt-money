595c5d82a4ba7fe049eaa855521d663f8b57b943
Gabriel Amorim <therealamorim@gmail.com>
2022-10-04 12:11:52 -0300

    project setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { GlobalStyle } from "./styles/global";
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "./styles/themes/default";
+
+export function App() {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+      <h1>Hello World</h1>
+    </ThemeProvider>
+  )
+}
