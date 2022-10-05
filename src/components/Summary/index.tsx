a60c571b13c320be3878284ef22b20581bf1aa63
Gabriel Amorim <therealamorim@gmail.com>
2022-10-05 02:05:24 -0300

    calculate summary values

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -7,6 +7,22 @@ import { useContext } from 'react';
 export function Summary() {
   const { transactions } = useContext(TransactionsContext);
   
+  const summary = transactions.reduce((acc, transaction) => {
+    if (transaction.type === 'income') {
+      acc.incomes += transaction.price;
+      acc.total += transaction.price;
+    } else {
+      acc.outcomes += transaction.price;
+      acc.total -= transaction.price;
+    }
+    
+    return acc;
+  }, {
+    incomes: 0,
+    outcomes: 0,
+    total: 0,
+  });
+
   return (
     <SummaryContainer>
       <SummaryCard>
@@ -15,7 +31,7 @@ export function Summary() {
           <ArrowCircleUp size={32} color={"#00b37e"} />
         </header>
 
-        <strong>R$ 17.400,00</strong>
+        <strong>{summary.incomes}</strong>
       </SummaryCard>
 
       <SummaryCard>
@@ -24,7 +40,7 @@ export function Summary() {
           <ArrowCircleDown size={32} color={"#f75a68"} />
         </header>
 
-        <strong>R$ 17.400,00</strong>
+        <strong>{summary.outcomes}</strong>
       </SummaryCard>
 
       <SummaryCard variant='green'>
@@ -33,7 +49,7 @@ export function Summary() {
           <CurrencyDollar size={32} color={"#fff"} />
         </header>
 
-        <strong>R$ 17.400,00</strong>
+        <strong>{summary.total}</strong>
       </SummaryCard>
     </SummaryContainer>
   )
